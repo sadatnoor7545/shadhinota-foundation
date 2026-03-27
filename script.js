1909318e263e9275a3e81293f8fc3ea568e9821a
sadatnoor7545 <sadatnoor555@gmail.com>
2026-03-28 01:11:47 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
-function donate() {
-    alert("Send bKash to +8801700915545");
+function toggleDark(){
+    document.body.classList.toggle("dark");
+}
+
+/* LANGUAGE */
+let lang="en";
+
+function toggleLang(){
+    if(lang==="en"){
+        document.getElementById("title").innerText="সবার জন্য স্বাধীনতা";
+        document.getElementById("subtitle").innerText="দারিদ্র্য ও ক্ষুধামুক্ত সমাজ গড়ি";
+        document.getElementById("impactTitle").innerText="আমাদের প্রভাব";
+        document.getElementById("p1").innerText="মানুষকে সহায়তা";
+        document.getElementById("p2").innerText="স্বেচ্ছাসেবক";
+        document.getElementById("p3").innerText="প্রকল্প";
+        lang="bn";
+    } else {
+        location.reload();
+    }
+}
+
+/* COPY NUMBER */
+function copyNumber(number){
+    navigator.clipboard.writeText(number);
+    alert("Number copied: " + number);
+}
+
+/* NAV */
+function scrollTop(){
+    window.scrollTo({top:0, behavior:'smooth'});
+}
+
+function goHome(){
+    window.location.href="index.html";
 }
